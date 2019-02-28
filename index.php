2b27b6a4e8ceb69c175f2df72a700e1cfb071116
Elaborata <Elaborata@users.noreply.github.com>
2019-02-28 20:06:36 -0300

    Teste Commit GitHub Desktop
    Testando

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Curso PHP 11088</title>
+	</head>
+
+	<body>
+		<h1>Curso PHP 11088</h1>
+
+		<hr />
+
+		<ul>
+			<li>
+				<a href="lessons/20190227/aula-2702.php" target="_self">Aula 27/fev</a>
+			</li>
+			<li>
+				<a href="lessons/20190228/index.php" target="_self">Aula 28/fev</a>
+			</li>
+		</ul>
+
+		<hr />
+
+		<?php echo "Teste PHP - Funcionando";  ?>
+
+		<?php
+			echo "<hr />";
+
+			$x = "AAA";
+
+			echo "<h4>aaaa</h4>";
+			echo "
+				<table>
+					<tr>
+						<th>Coluna 1</th>
+						<th>Coluna 2</th>
+						<th>Coluna 3</th>
+					</tr>
+				</table>
+			";
+
+			echo $x . $x . " $x";
+
+			echo "<hr />";
+
+		?>
+
+		<p>This is a paragraph.</p>
+
+	</body>
+
+</html>
---
+++ lessons/20190227/aula-2702.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Aula 27/Fev</title>
+	</head>
+
+	<body>
+    <form id="frmAula2702" name="frmAula2702" method="get" action="aula-2702-proc.php">
+      <label for="txtName">Nome:</label><br>
+      <input type="text" id="txtName" name="txtName"><br>
+      <label for="txtSobrenome">Sobrenome:</label><br>
+      <input type="text" id="txtSobrenome" name="txtSobrenome"><br>
+      <input type="submit" id="subOk" name="subOk" value="Ok">
+    </form>
+
+  </body>
+</html>
